3f547c455aa4d1196e9eb2f8c2311a07f870a248
tarundemotest <job.tarun360@gmail.com>
2018-09-18 19:09:47 +0530

    Layout test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::resource('films', 'FilmsController');
+
+Route::get('/home', 'HomeController@index')->name('home');
